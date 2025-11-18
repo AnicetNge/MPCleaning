e71a72e42f7cbcf84b16a426202a2181ad594387
anicet nge <ngeanicet@gmail.com>
2025-11-18 13:06:49 +0100

    ajout de la navigation mobile et du script gerant le hamburger

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,6 @@
-console.log("script lie");
+// Menu mobile
+const toggle = document.getElementById("menu-toggle");
+const navbar = document.getElementById("navbar");
+toggle.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
